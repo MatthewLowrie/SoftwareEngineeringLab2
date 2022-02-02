9cb724457fef1c27715a02fb88ce935502a95989
MatthewLowrie <98457937+MatthewLowrie@users.noreply.github.com>
2022-02-02 10:25:02 +0000

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,14 @@
+document.write("Hello World!");
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>MY Changing Web Page!</title>
+
+        <script src = "script1.js"></script>
+
+        <body>
+            
+        </body>
+    </head>
+</html>
